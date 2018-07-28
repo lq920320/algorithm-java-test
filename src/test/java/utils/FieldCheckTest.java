6fbc06525043885ce24e1f3e717052994dd325d5
lq <liuqian920320@163.com>
2018-07-28 20:42:15 +0800

    update get fields test

--- src/test/java/utils/FieldCheckTest.java
+++ src/test/java/utils/FieldCheckTest.java
@@ -3,6 +3,7 @@ package utils;
 import lombok.Data;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,47 @@ public class FieldCheckTest {
 
   @Test
   public void fieldCheckTest() {
+    getManList().forEach(man -> {
+      if (FieldCheckUtils.fieldHasNull(man)) {
+        System.out.println(man.getName() + "  " + "has null field!");
+      }
+      if (FieldCheckUtils.fieldHasNoneNull(man)) {
+        System.out.println(man.getName() + "  " + "has none null field!");
+      }
+    });
+
+  }
+
+  @Test
+  public void getFieldTest() {
+    getManList().forEach(man -> {
+      // 当所有的属性都为public的时候，Class.getFields()才能获取得到，此时的fields.length才不为0
+      Field[] fields = man.getClass().getFields();
+      System.out.println("fields length is: " + fields.length);
+      for (Field field : fields) {
+        try {
+          field.setAccessible(true);
+          if (field.get(man) == null) {
+            System.out.println(field.getName() + " is null!");
+          }
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+
+  @Data
+  public class Man {
+    public String name;
+    public Integer age;
+    public Float high;
+    public String gender;
+    public Float weight;
+  }
+
+  private List<Man> getManList() {
     Man man1 = new Man() {{
       setName("A");
       setAge(24);
@@ -39,31 +81,12 @@ public class FieldCheckTest {
     Man man5 = new Man() {{
       setName("E");
     }};
-    List<Man> manList = new ArrayList<Man>() {{
+    return new ArrayList<Man>() {{
       add(man1);
       add(man2);
       add(man3);
       add(man4);
       add(man5);
     }};
-    manList.forEach(man -> {
-      if (FieldCheckUtils.fieldHasNull(man)) {
-        System.out.println(man.getName() + "  " + "has null field!");
-      }
-      if (FieldCheckUtils.fieldHasNoneNull(man)) {
-        System.out.println(man.getName() + "  " + "has none null field!");
-      }
-    });
-
-  }
-
-
-  @Data
-  private class Man {
-    private String name;
-    private Integer age;
-    private Float high;
-    private String gender;
-    private Float weight;
   }
 }
